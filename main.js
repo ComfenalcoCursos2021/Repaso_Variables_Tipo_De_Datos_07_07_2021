ee0498ee03240b63c17c9aa8221da0261cca5659
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-07-07 20:59:40 -0500

    Repaso de las variables y tipo de datos 07072021

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+nombreCompletoDelProfesor = `Miguel Angel Castro Escamilla :V`;
+miEdad = 23;
+// altura = 1.43;
+soyUnProfesor = true;
+
+
+console.log(nombreCompletoDelProfesor, typeof(nombreCompletoDelProfesor));
+console.log(miEdad, typeof(miEdad));
+// console.log(altura, typeof(altura));
+console.log(soyUnProfesor, typeof(soyUnProfesor));
+
+
+// alert()
+// document.write();
